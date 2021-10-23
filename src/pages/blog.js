97dbd9c7e17ff72cee3f0bed6b1baeb4e75ad753
thilakmani-dev <thilakmani777@psnacet.edu.in>
2021-10-23 23:36:27 +0530

    added single blog

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import {
+  BlogCard,
+  GridContainer,
+  HeaderThree,
+} from '../components/Projects/ProjectsStyles';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
 
@@ -7,6 +12,19 @@ function Blog() {
     <Layout>
       <Section grid>
         <h2>Blogs will be posted soon</h2>
+        <GridContainer>
+          <BlogCard>
+            <HeaderThree title='Post'>Async & Defer</HeaderThree>
+            <p>Posted At : October 19 2021</p>
+            <ExternalLinks
+              href={
+                'https://thilakmanib.hashnode.dev/async-and-defer-in-javascript'
+              }
+            >
+              Read More
+            </ExternalLinks>
+          </BlogCard>
+        </GridContainer>
       </Section>
     </Layout>
   );
